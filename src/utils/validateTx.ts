f583d9449286cf51e452b7359ef2c265ae7cf2d1
himandrishram27 <himandrisharma27@gmail.com>
2022-01-11 17:11:39 +0530

    added conditions in comparison of data

--- src/utils/validateTx.ts
+++ src/utils/validateTx.ts
@@ -12,12 +12,14 @@ export function validateTx(txData:TxData):Promise<string>{
                     if(v && v.args.data) {   
                         let comparingEntity = Web3.utils.hexToUtf8(v.args.data)
                         comparingEntity = JSON.parse(comparingEntity)
-                        if(comparingEntity.invoiceId===txData.invoiceId || v.args.data===txData.data){
+                        let amount = parseInt(v.args.amount._hex,16)
+                        if((comparingEntity.invoiceId===txData.invoiceId || v.args.data===txData.data) &&
+                                v.args.to===txData.recipient && amount===txData.amount){
                             return true;
-                        }        
+                        }
                     }
                  })
-                 value.map((v:any)=>{if(v)result=true;});
+                 value.map((v:any)=>{if(v==true)result=true;});
                  return result;
              })
              .catch((err:any)=>{
--- test/test.js
+++ test/test.js
@@ -18,7 +18,11 @@ describe('generatePaymentQR function test', () => {
 });
 describe('validateTx function test', () => {
     it('should return an object', async function() {
-        var result = await index.validateTx({txHash:'0xc852028aa1064bc3e15b8c5f2a71fc376398d98a8edf8166cdd1d646fe7054f4',recipient:'0xee724540706296ebad65aea2515efe0949f97ae6',amount:'22200',invoiceId:'receipt-id-3938383',category:'Digital Services',data:'bleh'});
+        var result = await index.validateTx({txHash:'0xc852028aa1064bc3e15b8c5f2a71fc376398d98a8edf8166cdd1d646fe7054f4',recipient:'0x2FaDA5F33728761648dAB4BE25BF0fE831B9dD34',amount:500,invoiceId:'receipt-id-3938383',category:'Digital Services',data:'bleh'});
         console.log(result);
     });
 });
+// from: '0x1251ea725C8cB898BB3C0Ca08B8945185a78828f',
+// to: '0x2FaDA5F33728761648dAB4BE25BF0fE831B9dD34',
+// amount: [BigNumber],
+// data: '0x7b2263616c6c6261636b55726c223a2268747470733a2f2f736f6d6576656e646f722e636f6d2f6170692f6d7963616c6c6261636b656e64706f696e74222c2022696e766f6963654964223a22726563656970742d69642d33393338333833222c0a2277656273697465223a2268747470733a2f2f736f6d6576656e646f722e636f6d222c0a2276656e646f724e616d65223a2022536f6d652056656e646f72220a7d'
